8e0669d6c90dca664e3269057c34dcfa7a76cefd
Maxmud Axmedov <axmedovmaxmud559@gmail.com>
2023-04-11 17:36:19 +0500

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import Button from './components/Button';
+import "./main.css"
+export default function App() {
+  const [value, setValue] = useState('');
+  const inputRef = useRef('');
+  const textRef = useRef('');
 
-function App() {
+  function handleSubmit(){
+    setValue(inputRef.current.value);
+
+  }
+  function handleBold(){
+    textRef.current.classList.toggle("italic");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container'>
+      <form className='w-50 m-auto mt-5 d-flex justify-content-between'  >
+        <input className='form-control w-75' type='text' ref={inputRef} onChange={handleSubmit}/>
+        <button className='btn btn-success'>submit</button>
+      </form>
+      <div>
+        <Button className="btn btn-success">Bold</Button>
+        <Button className="btn btn-danger">Italic</Button>
+        <Button className="btn btn-info">Underline</Button>
+      </div>
+      <h2 ref={textRef}>{value}</h2>
+    </div> 
+  )
 }
-
-export default App;
